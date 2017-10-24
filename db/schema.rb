585aeaa6198500e7fdae6025871b6b2ff4fa3db6
Dynee <lucaspadden@gmail.com>
2017-10-24 14:49:32 -0400

    Added items scaffold for testing JWT authentication, everything works fine now

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024163417) do
+ActiveRecord::Schema.define(version: 20171024183324) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
